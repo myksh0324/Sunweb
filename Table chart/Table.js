c7fd39bfc564321345e2a310517f983f30ed01fa
Sun <myksh0324@gmail.com>
2018-03-26 10:42:32 +0900

    Edit Table

--- Table chart/Table.js
+++ Table chart/Table.js
@@ -1,9 +1,12 @@
+function PreviewImage() {
+	var oFReader = new FileReader();
+	oFReader.readAsDataURL(document.getElementById("Image_Upload").files[0]);
 
-    function PreviewImage() {
-        var oFReader = new FileReader();
-        oFReader.readAsDataURL(document.getElementById("Image_Upload").files[0]);
+	oFReader.onload = function (oFREvent) {
+		document.getElementById("Image_Preview").src = oFREvent.target.result;
+	};
+};
 
-        oFReader.onload = function (oFREvent) {
-            document.getElementById("Image_Preview").src = oFREvent.target.result;
-        };
-    };
+//var password document.getElementById("Password"), confirmPassword document.getElementById("Confirm_Password");
+
+console.log("Finish");
